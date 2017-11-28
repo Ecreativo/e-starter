fc08760f2049d52175555ec5afd7419fa19fcd62
Camiloruiiz <camiloruiz@esquemacreativo.com>
2017-11-28 12:17:32 +0000

    [fix] Flickity

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,24 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin")
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-  filename: "styles.css",
+  filename: "static/css/main.css",
 });
 
-module.exports = {
+const isProduction = (process.env.NODE_ENV === 'production');
+
+let config = {
   entry: {
     scripts: [
       './_assets/javascripts/application.js',
       './_assets/javascripts/bootstrap.js',
+      'flickity'
     ],
   },
   output: {
     filename: 'static/js/[name].js',
-    path: path.resolve(__dirname, 'site.com/src/')
+    path: path.resolve(__dirname, 'site.com/public_html/')
   },
-  //devtool: "source-map",
+  devtool: "source-map",
   module: {
     rules: [
       // Include pug-loader to process the pug files
@@ -45,10 +48,16 @@ module.exports = {
           //resolve-url-loader may be chained before sass-loader if necessary
           use: [{
             loader: "css-loader",
-            options: { sourceMap: true, includeContent: true } // translates CSS into CommonJS
+            options: { sourceMap: true } // translates CSS into CommonJS
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              config: { path: __dirname},
+              sourceMap: true
+            }
           }, {
             loader: "sass-loader",
-            options: { sourceMap: true, includeContent: true } // compiles Sass to CSS
+            options: { sourceMap: true } // compiles Sass to CSS
           }]
         })
       },
@@ -59,7 +68,8 @@ module.exports = {
     ]
   },
   context: path.resolve(__dirname, './site.com/src'),
-  plugins: [
+  plugins: isProduction ? [
+    new UglifyJSPlugin(),
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
@@ -77,5 +87,28 @@ module.exports = {
       template: './index2.pug'
     }),
     extractSass
+  ] : [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+      // In case you imported plugins individually, you must also require them here:
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
+      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+      Flickity: "flickity"
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './index.pug',
+      filename: '../public_html/index.html'
+    }),
+    extractSass
   ]
-};
+}
+
+//module.exports = { config };
+
+module.exports = config;
