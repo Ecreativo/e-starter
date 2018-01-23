6964df745feff6ffbd3806c6c1f299b351b2ea0d
Camiloruiiz <camiloruiz@esquemacreativo.com>
2018-01-23 16:17:08 +0000

    [remove] remplace pluging

--- tasks/pro/html.js
+++ tasks/pro/html.js
@@ -1,22 +1,18 @@
 import gulp from 'gulp'
 import htmlmin from 'gulp-htmlmin'
-import replace from 'gulp-replace'
 import notify from 'gulp-notify'
 import size from 'gulp-size'
-const config = require('../config').html;
+const config = require('../config').html
 
 /**
  * Minimize HTML
  */
 gulp.task('html:production', function () {
-  var remplace = config.remplace;
-  var from = size();
-  var to = size();
+  var from = size()
+  var to = size()
   return gulp.src(config.src)
     .pipe(from)
     .pipe(htmlmin(config.options))
-    //.pipe(replace(remplace.css.x, remplace.css.y))
-    //.pipe(replace(remplace.js.x, remplace.js.y))
     .pipe(gulp.dest(config.dest))
     .pipe(to)
     .pipe(notify({
@@ -24,7 +20,7 @@ gulp.task('html:production', function () {
       subtitle: 'Optimized',
       onLast: true,
       message: function () {
-        return from.prettySize + ' → ' + to.prettySize;
+        return from.prettySize + ' → ' + to.prettySize
       }
-    }));
-});
+    }))
+})
