787701a57296ca5213ed2563571978b7a24e91f8
Camiloruiiz <camiloruiz@esquemacreativo.com>
2018-05-22 21:14:55 +0100

    [remove] old task and devpendencies

--- tasks/pro/img.js
+++ tasks/pro/img.js
@@ -8,21 +8,8 @@ const config = require('../config').images.production
  * Copy and minimize image files
  */
 export function images(done) {
-  var from = $.size()
-  var to = $.size()
   return gulp.src(config.src)
-    .pipe(from)
     .pipe($.cache($.imagemin(config.options)))
     .pipe(gulp.dest(config.dest))
-    .pipe(to)
-    .pipe($.notify({
-      title: 'Images',
-      subtitle: 'Optimized',
-      onLast: true,
-      wait: true,
-      message: function () {
-        return from.prettySize + ' → ' + to.prettySize
-      }
-    }))
   done()
 }
