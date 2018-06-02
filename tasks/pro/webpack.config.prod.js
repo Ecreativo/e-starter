d972a539ee0d885dcdaf587a0fd0852bb4cba8aa
Camiloruiiz <camiloruiz@esquemacreativo.com>
2018-06-03 00:54:03 +0100

    [drop] uglifyjs-webpack-plugin

--- tasks/pro/webpack.config.prod.js
+++ tasks/pro/webpack.config.prod.js
@@ -3,13 +3,13 @@ import webpack from 'webpack'
 
 import merge from 'webpack-merge'
 import common from '../webpack.js'
-import UglifyJSPlugin from 'uglifyjs-webpack-plugin'
 import FaviconsWebpackPlugin from 'favicons-webpack-plugin'
 
 export let config = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   optimization: {
+    minimize: true,
     splitChunks: {
       cacheGroups: {
         vendor: {
@@ -22,17 +22,7 @@ export let config = merge(common, {
     }
   },
   plugins: [
-    new UglifyJSPlugin({
-      sourceMap: false,
-      uglifyOptions: {
-        ecma: 6,
-        mangle: false,
-        warnings: true,
-        compress: false,
-        safari10: true,
-        ie8: true
-      }
-    }),
+    
     // Make module IDs more stable
     new webpack.HashedModuleIdsPlugin(),
     // Concatenate modules where possible
